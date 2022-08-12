dd86d16f033f363ba0d9344efbec2904efe5187d
Felipe Mendonca <felipemom2001@gmail.com>
2022-08-12 18:05:49 +0200

    refactor: add faker-js to remoteAuthentication test

--- src/data/usecases/authentication/remoteAuthentication.test.ts
+++ src/data/usecases/authentication/remoteAuthentication.test.ts
@@ -1,6 +1,8 @@
 import { HttpPostClientSpy } from '@/data/mocks/mockHttpClient';
 import { RemoteAuthentication } from './remoteAuthentication';
 
+import { faker } from '@faker-js/faker';
+
 const makeSut = (url = 'any_url') => {
   const httpPostClientSpy = new HttpPostClientSpy();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
@@ -10,7 +12,7 @@ const makeSut = (url = 'any_url') => {
 
 describe('RemoteAuthentication', () => {
   it('Should call HttpPostClient with correct URL', () => {
-    const url = 'other_url';
+    const url = faker.internet.url();
     const { httpPostClientSpy, sut } = makeSut(url);
     sut.auth();
     expect(httpPostClientSpy.url).toBe(url);
